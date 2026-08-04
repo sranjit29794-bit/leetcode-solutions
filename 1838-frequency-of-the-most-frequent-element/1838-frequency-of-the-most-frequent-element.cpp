3791c645b8a9e85e0a0818f6477c2ef4bba14167
code-ninja-rs <sranjit29794@gmail.com>
2026-08-04 19:40:16 +0530

    Time: 53 ms (13.74%), Space: 105.8 MB (63.94%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -6,8 +6,8 @@ public:
         long long int windowSum=0;
         int left=0;
         int ans=1;
-
-        for(int right=0;right<nums.size();right++){
+        int s=nums.size();
+        for(int right=0;right<s;right++){
             windowSum+=nums[right];
             while(((long long)nums[right]*(right-left+1)-windowSum)>k){
                 windowSum-=nums[left];
