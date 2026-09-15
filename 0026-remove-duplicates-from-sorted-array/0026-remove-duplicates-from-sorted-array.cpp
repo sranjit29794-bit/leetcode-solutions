def23c9bcc4a20de79b5d85d795c61c2ddad5682
code-ninja-rs <sranjit29794@gmail.com>
2026-09-15 18:55:43 +0530

    Comment out alternative removeDuplicates method
    Commented out the second method for removing duplicates from a sorted array.

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -13,3 +13,28 @@ public:
         return k;
     }
 };
+
+//2nd method
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         vector<int>expectedNums;
+
+//         int key=INT_MIN;
+//         int count=0;
+
+//         for(auto x:nums){
+
+//             if(x!=key) {
+//                 expectedNums.push_back(x);
+//                 key=x;
+//                 count++;
+
+//             }
+//         }
+
+//         nums=expectedNums;
+
+//         return count;
+//     }
+// };
