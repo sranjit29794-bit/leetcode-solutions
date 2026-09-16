e6f141aee2e9ea6f797e38119e5e82cc467cf34b
code-ninja-rs <sranjit29794@gmail.com>
2026-09-16 20:26:52 +0530

    Time: 90 ms (15.55%), Space: 113.3 MB (20.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-
-        unordered_set<int> s(nums.begin(),nums.end());
+               unordered_set<int> s(nums.begin(),nums.end());
 
         return s.size()!=nums.size();
+
     }
 };
