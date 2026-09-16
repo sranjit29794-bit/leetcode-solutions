06471c6e9ffdc7003d6c5674eaef9e942886fef8
code-ninja-rs <sranjit29794@gmail.com>
2026-09-16 19:30:12 +0530

    Add alternative max profit solution as comments
    Added commented-out alternative solution for reference.

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -24,3 +24,11 @@ public:
         return maxProfit;
     }
 };
+
+//east solution
+// int maxProfit=0;
+
+// for(int i=1;i<prices.size();i++){
+
+//     if(prices[i]>prices[i-1] maxProfit=prices[i]-prices[i-1];
+// }
